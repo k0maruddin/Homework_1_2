8adae0dade641a3780479333784ac7d8c46f147f
Komaruddin <komaruddin5784@gmail.com>
2023-02-16 15:20:51 +0700

    upload tugas

---
+++ soal_1.js
@@ -0,0 +1,16 @@
+let a = "hello world!"
+let b = {}
+
+// Split array ke karakter
+let d = a.split("")
+
+// hitung perkarakter
+for (let i = 0; i < d.length; i++) {
+  let e = d[i];
+  b[e] = (b[e] || 0) + 1
+}
+
+// Output jumlah karakter
+for (let e in b) {
+  console.log(e + ": " + b[e])
+}
---
+++ soal_2.js
@@ -0,0 +1,21 @@
+let namaBilangan = "1000000000"
+
+switch(namaBilangan) {
+    case "5" :
+        console.log("SATUAN")
+        break
+    case "10" :
+        console.log("PULUHAN")
+        break
+    case "100" :
+        console.log("RATUSAN")
+        break
+    case "1000" :
+        console.log("RIBUAN")
+        break
+    case "20000" :
+        console.log("PULUHRIBUAN")
+        break
+    default :
+        console.log("Input Tidak Valid")         
+}
